9a2d6250b0c82f03274ccde734a2def09840f016
AlexZerr <thwarts@gmail.com>
2018-06-03 02:39:28 -0600

    I think we need to move the deal into initialize

--- lib/card_game.ex
+++ lib/card_game.ex
@@ -14,22 +14,20 @@ defmodule CardGame do
   defp initialize(game = %Game{}) do
     deck = Cards.create_deck
     game = initialize_players(%{game | deck: deck})
-    #    for player <- game.player_count do
-    #end
-    IO.inspect game
-    { hand, deck } = Cards.deal(deck, @default_hand_size)
-     
-    loop(game)
+    #IO.inspect game
+    IO.puts "this is the game"
+    for player <- game.players do
+      {hand, deck} = Cards.deal(deck, 5)
+      %{player | hand: hand}
+    end
+
+    #loop(game)
   end
 
   def initialize_players(game = %Game{}) do
     IO.puts "Playercount: #{game.player_count}"
     players = create_players([], game.player_count)
     IO.inspect players
-    #for player <- players do
-    #{ hand, deck } = Cards.deal(game.deck)
-    players = Enum.map(players, fn x ->  
-        #end
     %{game | players: players}
     #for player <- game.player_count do
     #IO.puts player
